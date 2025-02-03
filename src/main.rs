4127015d3f9b60b1e31d0c4c5a2992b1f0b95c64
mwcz <mwc@clayto.com>
2025-02-03 13:59:40 -0500

    perf: speed things up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
-use std::env;
-use std::process::Command;
+
+use std::process::{Command, Output};
+use std::{env, io};
 use termion::{color, style};
 
 const USAGE: &str = "USAGE: depfirstsearch REGEX";
@@ -11,21 +12,33 @@ fn main() {
         std::process::exit(1);
     };
 
+    let Ok(output): io::Result<Output> = Command::new("cargo").args(["metadata"]).output() else {
+        eprintln!("Failed to execute `cargo metadata`; are you in a cargo workspace?");
+        std::process::exit(1);
+    };
+
+    if !output.status.success() {
+        eprintln!("Failed to execute `cargo metadata`; are you in a cargo workspace?");
+        std::process::exit(1);
+    }
+
+    let Ok(metadata): Result<serde_json::Value, _> = serde_json::from_slice(&output.stdout) else {
+        eprintln!("Could not parse `cargo metadata` output.");
+        std::process::exit(1);
+    };
+
     let is_stdout_tty = termion::is_tty(&std::fs::File::create("/dev/stdout").unwrap());
-
-    let output = Command::new("cargo")
-        .args(["metadata"])
-        .output()
-        .expect("Failed to execute cargo metadata");
-
-    let metadata: serde_json::Value =
-        serde_json::from_slice(&output.stdout).expect("Failed to parse cargo metadata output");
 
     let workspace_members = metadata["packages"]
         .as_array()
         .expect("Failed to extract workspace members");
 
-    let mut all_crates = Vec::new();
+    let keyword_re = Regex::new(&search_term).unwrap();
+
+    let size_estimate = workspace_members.len() / 10;
+
+    let mut all_crates = Vec::with_capacity(size_estimate);
+
     for member in workspace_members {
         let name = member["name"].as_str().unwrap_or("");
         let version = member["version"].as_str().unwrap_or("");
@@ -57,21 +70,19 @@ fn main() {
             format!("{name}@{version}  {keywords}\n\t{description}")
         };
 
-        all_crates.push(crate_msg);
+        if keyword_re.is_match(name)
+            || keyword_re.is_match(version)
+            || keyword_re.is_match(&keywords)
+            || keyword_re.is_match(&description)
+        {
+            all_crates.push(crate_msg);
+        }
     }
 
-    let keyword_re = Regex::new(&search_term).unwrap();
-
-    let matching_crates = all_crates
-        .iter()
-        .filter(|&cr| keyword_re.is_match(cr))
-        .map(|cr| cr.trim())
-        .collect::<Vec<&str>>();
-
     // Search for the user's term in the collected output.
-    if matching_crates.is_empty() {
+    if all_crates.is_empty() {
         println!("No crates found matching {search_term}");
     } else {
-        println!("{}", matching_crates.join("\n"));
+        println!("{}", all_crates.join("\n"));
     }
 }
